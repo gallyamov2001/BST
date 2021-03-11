c16677a11411628f38d5b708d885eb094ead3045
Islam <galliamov.im@phystech.edu>
2021-03-11 23:21:52 +0300

    Fixed test.c file

--- test.c
+++ test.c
@@ -3,14 +3,18 @@
 
 
 int main(){
-    struct Node* tree;
-    int nums[] = {17, 23, 55, 34, 11, 99, 54, 24, 100, 9};
-    for (int i = 0; i < sizeof(nums); i++){
+    struct Node* tree = NULL;
+    const int n = 10;
+    int nums[n] = {17, 23, 55, 34, 11, 99, 54, 24, 100, 9};
+    for (int i = 0; i < n; i++){
         tree = tree_add(tree, nums[i]);
     }
+    printf("Tree is: ");
     tree_print(tree);
-    fprintf(stdout, "max is %d\n", max(tree)->data);
-    fprintf(stdout, "min is %d\n", min(tree)->data);
+    printf("\n");
+    fprintf(stdout, "Max is %d\n", max(tree)->data);
+    fprintf(stdout, "Min is %d\n", min(tree)->data);
     tree = delete(tree, 55);
+    printf("Tree after deleting 55 is: ");
     tree_print(tree);
 }
